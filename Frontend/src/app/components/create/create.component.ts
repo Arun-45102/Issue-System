cee190533c712536c6444c248398d20987b9bb6c
Arun-45102 <30484922+Arun-45102@users.noreply.github.com>
2018-09-13 21:15:53 +0530

    Add files via upload

---
+++ Frontend/src/app/components/create/create.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IssueService } from '../../issue.service';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  createForm: FormGroup;
+
+  constructor(private issueService: IssueService, private fb: FormBuilder, private router: Router, private snackBar: MatSnackBar) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: ''
+    });
+   }
+
+   addIssue(title, responsible, description, severity) {
+     this.issueService.addIssue(title, responsible, description, severity).subscribe(() => {
+      this.snackBar.open('Issue Created Successfully', 'OK', {
+        duration: 3000
+      });
+       this.router.navigate(['/list']);
+     });
+   }
+
+  ngOnInit() {
+  }
+
+}
